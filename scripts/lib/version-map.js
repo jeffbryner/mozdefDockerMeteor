b2ae0f6fe5675c99e1e1a2fed669d1dd826089b1
Jeff Bryner <jeff@jeffbryner.com>
2016-12-19 10:37:58 -0800

    add supporting scrips

---
+++ scripts/lib/version-map.js
@@ -0,0 +1,56 @@
+module.exports = {
+  "METEOR@1.4.1.1": {
+    node: "4.4.7",
+    recommendedDockerImages: [
+      "abernix/spaceglue:node-4.6.1",
+      "abernix/spaceglue:node-4.6.2",
+      "abernix/spaceglue:node-4.7.0",
+    ],
+  },
+  "METEOR@1.4.1.2": {
+    node: "4.6.0",
+    recommendedDockerImages: [
+      "abernix/spaceglue:node-4.6.1",
+      "abernix/spaceglue:node-4.6.2",
+      "abernix/spaceglue:node-4.7.0",
+    ],
+  },
+  "METEOR@1.4.1.3": {
+    node: "4.6.1",
+    recommendedDockerImages: [
+      "abernix/spaceglue:node-4.6.1",
+      "abernix/spaceglue:node-4.6.2",
+      "abernix/spaceglue:node-4.7.0",
+    ],
+  },
+  "METEOR@1.4.1.3": {
+    node: "4.6.1",
+    recommendedDockerImages: [
+      "abernix/spaceglue:node-4.6.1",
+      "abernix/spaceglue:node-4.6.2",
+      "abernix/spaceglue:node-4.7.0",
+    ],
+  },
+  "METEOR@1.4.2.1": {
+    node: "4.6.1",
+    recommendedDockerImages: [
+      "abernix/spaceglue:node-4.6.1",
+      "abernix/spaceglue:node-4.6.2",
+      "abernix/spaceglue:node-4.7.0",
+    ],
+  },
+  "METEOR@1.4.2.2": {
+    node: "4.6.2",
+    recommendedDockerImages: [
+      "abernix/spaceglue:node-4.6.2",
+      "abernix/spaceglue:node-4.7.0",
+    ],
+  },
+  "METEOR@1.4.2.3": {
+    node: "4.6.2",
+    recommendedDockerImages: [
+      "abernix/spaceglue:node-4.6.2",
+      "abernix/spaceglue:node-4.7.0",
+    ],
+  },
+};
---
+++ scripts/run_app.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+set -e
+my_dir=`dirname $0`
+path=`pwd`
+
+if [ -d /bundle ]; then
+  echo "=> Found /bundle"
+  tar -xz -C $HOME --no-same-owner --file /bundle/*.tar.gz
+  cd $HOME/bundle
+elif [ -d $HOME/bundle ]; then
+  echo "=> Found $HOME/bundle"
+  cd $HOME/bundle
+  tar -xz --no-same-owner --file $HOME/bundle/*.tar.gz
+  cd bundle/
+elif [ -n "$BUNDLE_URL" ]; then
+  echo "=> BUNDLE_URL is set"
+  cd $HOME/
+  curl -L -o bundle.tar.gz $BUNDLE_URL
+  tar -xz --no-same-owner --file bundle.tar.gz
+  cd bundle/
+elif [ -d $HOME/built_app ]; then
+  echo "=> Found $HOME/built_app"
+  cd $HOME/built_app
+elif [ -d /built_app ]; then
+  echo "=> Found /built_app"
+  cd /built_app
+else
+  echo "=> You don't have an meteor app to run in this image."
+  exit 1
+fi
+
+echo "=> Bundle Version"
+bundle_meteor_release="$(node ${my_dir}/lib/get_bundle_meteor_release)"
+echo " > ${bundle_meteor_release}"
+
+set +e
+echo "=> Proper Node Version"
+node ${my_dir}/lib/check_node_for_meteor ${bundle_meteor_release}
+case $? in
+  30)
+     echo "Unable to check valid version, newer?"
+     ;;
+  31)
+     echo "Invalid version of Node"
+     ;;
+  0)
+     echo "Correct Node Version"
+     ;;
+esac
+set -e
+
+echo "=> Actual Node Version"
+ACTUAL_NODE_VERSION="$(node --version | sed 's/^v//')"
+echo " > ${ACTUAL_NODE_VERSION}"
+
+# Set a delay to wait to start meteor container
+if [ -n "$DELAY" ]; then
+  echo "Delaying startup for $DELAY seconds"
+  sleep $DELAY
+fi
+
+if [ $INSTALL_NPM -eq 1 ]; then
+    echo "=> Executing NPM install within Bundle"
+    (cd programs/server && npm install)
+fi
+
+echo "=> meteor app ready "
+# node $NODE_OPTIONS main.js
+echo $NODE_OPTIONS
